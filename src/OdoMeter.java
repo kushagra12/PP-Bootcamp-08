4d2fb00536716291f811b0b1aac81df192ed642e
saurabh-code <saurabh.kumar.code@gmail.com>
2018-08-09 12:59:53 +0530

    added odometer interface

---
+++ src/OdoMeter.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class OdoMeter {
+	
+	private List<Integer> odoSeq;
+	
+	public OdoMeter(int size) {
+	}
+
+	public int next(int num) {
+		return 0;
+	}
+	
+	public int previous(int num) {
+		return 0;
+	}
+	
+	public int nextN(int num, int n) {
+		return 0;
+	}
+	
+	public int previousN(int num, int n) {
+		return 0;
+	}
+	
+	public int diff(int num1, int num2) {
+		return 0;
+	}
+	
+}
